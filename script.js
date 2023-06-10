568e4a1288e1b5c928f40b0ea389e0061785dd43
Jhonatas Reis <83553776+kira3434@users.noreply.github.com>
2023-06-10 00:53:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-function toggleNavbarItems() {
-    var navbarItems = document.getElementById("navbar-itens");
-    if (navbarItems.style.display === "none") {
-        navbarItems.style.display = "block";
-    } else {
-        navbarItems.style.display = "none";
-    }
-}
 
-document.addEventListener('DOMContentLoaded', function() {
-    var navbarLinks = document.querySelector('.navbar-links');
-    navbarLinks.addEventListener('click', toggleNavbarItems);
-});
+  function toggleLinksLateral(element) {
+    var linksLateral = document.querySelector('.links-lateral');
+    var navbarImg = document.getElementById('navbar-img');
+
+    // Verificar se a classe 'active' está presente
+    var isActive = linksLateral.classList.contains('active');
+
+    if (isActive) {
+      // Remover classe 'active' para ocultar a div links-lateral
+      linksLateral.classList.remove('active');
+
+      // Restaurar a imagem original da navbar-links
+      navbarImg.src = 'imagens-cafeteria/menu.png';   
+    } else {
+      // Adicionar classe 'active' para exibir a div links-lateral
+      linksLateral.classList.add('active');
+
+      // Alterar a imagem da navbar-links para imagem2.jpg
+      navbarImg.src = 'imagens-cafeteria/close.png';
+    }
+  }
